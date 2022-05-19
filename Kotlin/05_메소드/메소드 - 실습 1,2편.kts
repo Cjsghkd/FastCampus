cb29e32fd9ca2de5d63479193df3e998ee8653f0
Cjsghkd <pjhomg5530@naver.com>
2022-05-19 20:10:57 +0900

    메소드 - 실습 1,2편

--- Kotlin/05_메소드/메소드 - 실습 1,2편.kts
+++ Kotlin/05_메소드/메소드 - 실습 1,2편.kts
@@ -1,8 +1,13 @@
+var number100 : Int = 100
+
 // 함수를 선언하는 방법
 fun plusNumbers(first_num : Int, second_num : Int) : Int {
     val result : Int = first_num + second_num
     return result
 }
+
+// first_num = 200
+// println(first_num) -> 함수 안에서 사용하는 변수(파라미터)는 함수 범위(괄호)안에서만 사용가능하다
 
 // 함수를 사용(호출)하는 방법
 plusNumbers(first_num = 10, second_num = 20) // -> 30
@@ -40,3 +45,21 @@ fun plusNumbersWithNoReturn3(first_num : Int, second_num : Int) {
     return  // return만 쓰는 것은 되지만 반환값은 쓰면 안된다
 }
 plusNumbersWithNoReturn3(100, 200)
+
+// 함수선언을 간단하게 하는 방법
+fun shortPlusNumbers(firstNum : Int, secondNum : Int) = firstNum + secondNum
+val result4 : Int = shortPlusNumbers(firstNum = 10,secondNum = 100)
+println(result4)
+
+// 가변인자를 갖는 함수
+fun plusMultipleNumbers(vararg numbers : Int) : Unit {
+    for (number in numbers) {
+        println(number)
+    } // numbers에 들어온 값들을 하나하나 돌면서 println을 진행한다
+
+    // println(numbers)
+}
+plusMultipleNumbers(1, 2, 3, 4, 5)
+
+number100 = 200
+println(number100)
