99a36d3a539c3595222bd1abc69426f6f86fc713
Cjsghkd <pjhomg5530@naver.com>
2022-05-24 20:48:06 +0900

    과제 - 1

--- Kotlin/07~10_과제/과제 - 1.kts
+++ Kotlin/07~10_과제/과제 - 1.kts
@@ -1,10 +1,14 @@
 val inputs = listOf<Char>('A', 'B', 'E')
 val number : Int = 3
 
-for (i in 1..number) {
-    println(i)
-    for (j in inputs) {
-        print(j)
+fun abc(number : Int) {
+    for (i in 1..number) {
+        println(i)
+        for (j in inputs) {
+            print(j)
+        }
+        println("")
     }
-    println("")
 }
+
+abc(number)
