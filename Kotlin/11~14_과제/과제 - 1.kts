2662d5afc364a0a197ad9648cd62931c7f492514
Cjsghkd <pjhomg5530@naver.com>
2022-05-26 22:27:22 +0900

    과제 - 1

---
+++ Kotlin/11~14_과제/과제 - 1.kts
@@ -0,0 +1,17 @@
+fun plus(num1 : Int, num2 : Int) : Int {
+    return num1 + num2
+}
+fun minus(num1 : Int, num2 : Int) : Int {
+    return num1 - num2
+}
+fun multefe(num1 : Int, num2 : Int) : Int {
+    return num1 * num2
+}
+fun divide(num1 : Int, num2 : Int) : Int {
+    return num1 / num2
+}
+
+println(plus(5, 2))
+println(minus(5, 2))
+println(multefe(5, 2))
+println(divide(5, 2))
