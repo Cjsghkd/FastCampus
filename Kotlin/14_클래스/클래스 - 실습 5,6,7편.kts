04c383a3a1c1ae20efcc10db534903d7548a8595
Cjsghkd <pjhomg5530@naver.com>
2022-05-26 20:10:09 +0900

    클래스 - 실습 7편

--- Kotlin/14_클래스/클래스 - 실습 5,6,7편.kts
+++ Kotlin/14_클래스/클래스 - 실습 5,6,7편.kts
@@ -49,3 +49,24 @@ val book2_2 = Book2() // Book2 -> 붕어빵 틀(클래스), book2_2 -> 붕어빵
 println(book2_2.nextPage()) // 초기화 되지않았으므로 isInitialized가 false가 된다
 
 // book2 != book2_2 -> 서로 클래스는 같지만 각각의 객체이므로 객체마다 초기화를 해줘야한다
+
+// Lazy
+// - 호출시점에 by lazy 정의에 의해서 초기화을 수행한다
+// - val 에서만 사용 가능하다
+class Book3() {
+    val title : String by lazy {
+        // 본문 -> 다른 작업도 할 수 있지만 반드시 프로퍼티(속성)를 초기화 시켜주는 작업을 해야한다
+        println("lazy 초기화") // 다른 작업
+        // title = "책제목" -> 불가능
+        // return "책제목" -> 불가능
+        "책 제목" // 람다식이므로 마지막 줄이 리턴된다
+    }
+    // val title : String = "abc" -> lazy는 초기화 X
+}
+val book3 = Book3()
+//val book4 = Book4()
+//val book5 = Book4()
+//val book6 = Book4()   ->  복잡한 작업이 많고 속성중 title이 없다면 굳이 다 초기화 하지않고
+//val book7 = Book4()       lazy를 써서 필요할 때(사용할 때)만 초기화 시킬 수 있다
+//val book8 = Book4()
+println(book3.title)
