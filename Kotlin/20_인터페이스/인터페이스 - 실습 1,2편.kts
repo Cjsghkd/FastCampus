de3e90b06504b3b499c9fc92211fc1438f2356b8
Cjsghkd <pjhomg5530@naver.com>
2022-05-31 08:11:37 +0900

    인터페이스 - 실습 2편

---
+++ Kotlin/20_인터페이스/인터페이스 - 실습 1,2편.kts
@@ -0,0 +1,73 @@
+interface Tiger {
+    fun bite()
+    fun goToBox()
+}
+
+class backDoSanTiger : Tiger {
+    override fun bite() {
+        // 구체적인 기능
+    }
+
+    override fun goToBox() {
+        // 구체적인 기능
+    }
+}
+
+val backdosantiger : backDoSanTiger = backDoSanTiger()
+backdosantiger.bite()
+backdosantiger.goToBox()
+
+interface Person {
+    // 멤버들의 구현부가 없다
+    var dress : String // = "교복, 정장"
+    fun eat() // {}
+    fun sleep() {
+        println("잠을 잔다")
+    }
+}
+
+class Student : Person {
+    override var dress: String
+        get() = "교복"
+        set(value) {}
+
+    final override fun eat() { // final -> 이 클래스를 상속하는 클래스에서 override를 못하게 한다
+        println("밥을 먹는다")
+    }
+
+//    override fun sleep() {
+//        println("잠을 잔다")
+//    }
+
+    fun study() {
+        println("공부를 한다")
+    }
+}
+
+class GoodStudent : Student() {
+    override fun sleep() {
+        super.sleep()
+    }
+
+    override var dress: String
+        get() = super.dress
+        set(value) {}
+}
+
+class Teacher : Person {
+    override var dress: String
+        get() = "정장"
+        set(value) {}
+
+    override fun eat() {
+        TODO("밥을 먹는다")
+    }
+
+//    override fun sleep() {
+//        TODO("잠을 잔다")
+//    }
+}
+
+val student : Student = Student()
+student.eat()
+student.sleep()
