f3878f9dd152fd7a3362c05898792bb64498faf0
Cjsghkd <pjhomg5530@naver.com>
2022-05-30 22:52:39 +0900

    과제 - 풀이 3편

--- Kotlin/15~19 과제/과제 - 풀이 1,2,3편.kts
+++ Kotlin/15~19 과제/과제 - 풀이 1,2,3편.kts
@@ -21,7 +21,7 @@ open class Warrior constructor (hp : Int, var power : Int, var armor : Int) {
         this.hp = hp
     }
 
-    fun attack(monster : Monster) {
+    open fun attack(monster : Monster) {
         println("Warrior가 공격을 합니다")
         if (!monster.defense(this)) {
             // 몬스터 죽음
@@ -36,15 +36,42 @@ open class Warrior constructor (hp : Int, var power : Int, var armor : Int) {
         return this.isAlive
     }
 
-//    fun levelUp() : Knight {
-//
-//    }
+    fun levelUp() : Knight? {
+        if (this.killCount > 5) {
+            println("Knight로 레벨업!!")
+            return Knight(this)
+        } else {
+            println("레벨업을 할 수 없습니다 현재 킬 카운트 : ${this.killCount}")
+            return null
+        }
+    }
 
 }
 
-//class Knight : Warrior {
-//
-//}
+class Knight constructor (warrior: Warrior) : Warrior(warrior.hp, warrior.power, warrior.armor) {
+    var energy : Int = 0
+
+    init {
+        this.hp = hp
+        this.power = power + 2
+        this.armor = armor + 1
+    }
+
+    override fun attack(monster: Monster) {
+        super.attack(monster)
+        this.energy += 1
+    }
+
+    fun hardAttack(monster: Monster) {
+        if(monster.isAlive && this.energy >= 3) {
+//            this.power += this.power + 30
+//            monster.defense(warrior)
+//            this.power = this.power -= 30
+            monster.hp -= this.power + 30  // -> 좋은 방식은 아니다 (직접적으로 변수에 접근하는것 보단 함수로 접근 하는 것이 더 좋다 )
+            this.energy -= 3
+        } else println("에너지가 부족합니다. 에너지 : ${this.energy}")
+    }
+}
 
 class Monster constructor (hp : Int, var power : Int, var armor : Int) {
 
