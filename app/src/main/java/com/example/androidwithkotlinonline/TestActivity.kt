c76f9279576300fe6d73eae3e9210d189aad45e0
Cjsghkd <pjhomg5530@naver.com>
2022-06-08 17:54:18 +0900

    01_Activity - 실습 2편

--- app/src/main/java/com/example/androidwithkotlinonline/TestActivity.kt
+++ app/src/main/java/com/example/androidwithkotlinonline/TestActivity.kt
@@ -2,10 +2,12 @@ package com.example.androidwithkotlinonline
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
+        Log.d("LifeCycle", "onCreate")
     }
 }
