2279cc4ec697366e7e1128b9251066d5a0f49f8c
Cjsghkd <pjhomg5530@naver.com>
2022-06-08 19:41:14 +0900

    02_ViewControl - 실습 1편

---
+++ app/src/main/java/com/example/androidwithkotlinonline/ViewControl_02.kt
@@ -0,0 +1,19 @@
+package com.example.androidwithkotlinonline
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+
+class ViewControl_02 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_control02)
+
+        val textViewOne : TextView = findViewById(R.id.textViewOne)
+        val buttonOne : Button = findViewById(R.id.buttonOne)
+
+        Log.d("testOne", textViewOne.text.toString())
+    }
+}
