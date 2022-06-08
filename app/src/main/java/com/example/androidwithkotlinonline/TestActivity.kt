bd260f56c118d40bb9b317b7f3149933c4b97849
Cjsghkd <pjhomg5530@naver.com>
2022-06-08 18:08:02 +0900

    01_Activity - 실습 3편

--- app/src/main/java/com/example/androidwithkotlinonline/TestActivity.kt
+++ app/src/main/java/com/example/androidwithkotlinonline/TestActivity.kt
@@ -7,7 +7,39 @@ import android.util.Log
 class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_test)
-        Log.d("LifeCycle", "onCreate")
+        // 한번만 하면 되는 작업업
+       Log.d("LifeCycle", "onCreate")
+    }
+
+    override fun onStart() {
+        Log.d("LifeCycle", "onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d("LifeCycle", "onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d("LifeCycle", "onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d("LifeCycle", "onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d("LifeCycle", "onDestroy")
+        super.onDestroy()
+    }
+
+    override fun onRestart() {
+        Log.d("LifeCycle", "onRestart")
+        super.onRestart()
     }
 }
