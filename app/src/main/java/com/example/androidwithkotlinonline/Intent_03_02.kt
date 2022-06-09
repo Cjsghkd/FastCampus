70cdc7a5898fa4529ee082a05d4ba75b907e1e9a
Cjsghkd <pjhomg5530@naver.com>
2022-06-10 07:57:49 +0900

    03_Intent - 실습 3편

--- app/src/main/java/com/example/androidwithkotlinonline/Intent_03_02.kt
+++ app/src/main/java/com/example/androidwithkotlinonline/Intent_03_02.kt
@@ -2,10 +2,19 @@ package com.example.androidwithkotlinonline
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class Intent_03_02 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intent0302)
+
+        // Intent_03_02 액티비티를 호출한 Activity의 intent이다
+        val intent = intent
+        val data : String? = intent.extras?.getString("extra-data")
+        // val data1 : Int? = intent.extras?.getInt("extra-data")
+        if (data != null) {
+            Log.d("dataa", data)
+        }
     }
 }
