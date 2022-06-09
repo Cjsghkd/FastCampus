751b4af3f2890c8fc357bc77387fee97587bf7b8
Cjsghkd <pjhomg5530@naver.com>
2022-06-09 17:52:49 +0900

    과제 - 풀이 1편

---
+++ app/src/main/java/com/example/androidwithkotlinonline/Test_01_check.kt
@@ -0,0 +1,11 @@
+package com.example.androidwithkotlinonline
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Test_01_check : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test01_check)
+    }
+}
