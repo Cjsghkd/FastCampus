f6a40f20ece2fbcc5a4a14e27d35d51a9b24cbcc
Cjsghkd <pjhomg5530@naver.com>
2022-06-12 11:30:07 +0900

    05_ActivityStack - 실습 1편

---
+++ app/src/main/java/com/example/androidwithkotlinonline/Activity_05_01.kt
@@ -0,0 +1,26 @@
+package com.example.androidwithkotlinonline
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class Activity_05_01 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_0501)
+
+        (findViewById<TextView>(R.id.one)).setOnClickListener {
+            startActivity(Intent(this@Activity_05_01, Activity_05_01::class.java))
+        }
+
+        (findViewById<TextView>(R.id.two)).setOnClickListener {
+            startActivity(Intent(this@Activity_05_01, Activity_05_02::class.java))
+        }
+
+        (findViewById<TextView>(R.id.three)).setOnClickListener {
+            startActivity(Intent(this@Activity_05_01, Activity_05_03::class.java))
+        }
+
+    }
+}
