c69328c33f363e7d6b36f9ad8473dd58c9c4c812
Cjsghkd <pjhomg5530@naver.com>
2022-06-13 23:31:07 +0900

    06_과제 - 기본_실패(URL 연결)

---
+++ app/src/main/java/com/example/androidwithkotlinonline/Test_02_basic.kt
@@ -0,0 +1,24 @@
+package com.example.androidwithkotlinonline
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class Test_02_basic : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test02_basic)
+
+        val url : EditText = findViewById(R.id.url)
+        val button : Button = findViewById(R.id.searchButton)
+        val webview : WebView = findViewById(R.id.webView)
+
+        button.setOnClickListener {
+            webview.loadUrl(url.text.toString())
+        }
+    }
+}
