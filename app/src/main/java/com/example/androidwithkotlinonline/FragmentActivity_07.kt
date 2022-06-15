4ae9b4eff8e20a42e116b313daf87ca335e22400
Cjsghkd <pjhomg5530@naver.com>
2022-06-15 19:41:37 +0900

    07_Fragment - 실습 3편 (코드)

--- app/src/main/java/com/example/androidwithkotlinonline/FragmentActivity_07.kt
+++ app/src/main/java/com/example/androidwithkotlinonline/FragmentActivity_07.kt
@@ -2,10 +2,35 @@ package com.example.androidwithkotlinonline
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class FragmentActivity_07 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment07)
+
+        val fragmentManager = supportFragmentManager
+        val fragmentFirst = Fragment_01()
+
+        // Transaction
+        // - 작업의 단위 -> 시작과 끝이 있다
+        // A,B,C,D
+        (findViewById<TextView>(R.id.add)).setOnClickListener {
+            val transaction = fragmentManager.beginTransaction() // 시작
+            transaction.replace(R.id.root, fragmentFirst)
+            transaction.commit() // 끝
+        }
+
+        (findViewById<TextView>(R.id.remove)).setOnClickListener {
+
+        }
+
+
     }
+
+    // Commit
+    // 1> commit
+    // 2> commitAllowingStateLoss
+    // 3> commitNow
+    // 4> commitNowAllowingStateLoss
 }
