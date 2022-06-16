0561e2dd5908f0ea991492ff4327bacf5742c543
Cjsghkd <pjhomg5530@naver.com>
2022-06-16 21:09:56 +0900

    12_과제

---
+++ app/src/main/java/com/example/androidwithkotlinonline/Test_12.kt
@@ -0,0 +1,39 @@
+package com.example.androidwithkotlinonline
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+
+class Test_12 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test12)
+
+        var testItem = mutableListOf<Test>()
+        for (i in 0..10) {
+            testItem.add(Test("" + i + "번째 사람", "010-1111-111" + i))
+        }
+
+        val inflater = LayoutInflater.from(this)
+        val container = findViewById<LinearLayout>(R.id.container)
+        testItem.forEach {
+            val testItemView = inflater.inflate(R.layout.test_item, null)
+            val testImage = testItemView.findViewById<ImageView>(R.id.testImage)
+            val nthpeople = testItemView.findViewById<TextView>(R.id.peopleNumber)
+            val nthphone = testItemView.findViewById<TextView>(R.id.phoneNumber)
+
+            testImage.setImageDrawable(resources.getDrawable(R.drawable.blue_backgroud, this.theme))
+            nthpeople.text = it.peopleNumber
+            nthphone.text = it.phoneNumber
+            container.addView(testItemView)
+        }
+    }
+}
+
+class Test (val peopleNumber : String, val phoneNumber : String) {
+
+}
