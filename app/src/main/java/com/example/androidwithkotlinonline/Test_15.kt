f91aeede497885bcc9b0856e14595b012ce9faa8
Cjsghkd <pjhomg5530@naver.com>
2022-06-21 21:14:19 +0900

    15_과제 - 1단계 실패

---
+++ app/src/main/java/com/example/androidwithkotlinonline/Test_15.kt
@@ -0,0 +1,56 @@
+package com.example.androidwithkotlinonline
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class Test_15 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test15)
+
+        var chatList = mutableListOf<CHAT>()
+        for (i in 1..50){
+            chatList.add(CHAT("" + i + "번째 안녕하세요"))
+        }
+
+    }
+}
+
+class recyclerViewAdapter(
+    var chatList : MutableList<CHAT>,
+    var context : Context
+) : RecyclerView.Adapter<recyclerViewAdapter.ChatViewHolder>() {
+    inner class ChatViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+            var chatImage = view.findViewById<ImageView>(R.id.chatImage)
+            var chatText = view.findViewById<TextView>(R.id.chatText)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ChatViewHolder {
+        val inflater = LayoutInflater.from(context)
+        val view = inflater.inflate(R.layout.chat_item_list, parent)
+        return ChatViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ChatViewHolder, position: Int) {
+        holder.chatText.text = chatList.get(position).chatText
+    }
+
+    override fun getItemCount(): Int {
+        return chatList.size
+    }
+}
+
+
+class CHAT(val chatText : String) {
+
+}
