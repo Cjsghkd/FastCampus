77e151df9a7eb812586897b64b962b578a78a07d
Cjsghkd <pjhomg5530@naver.com>
2022-06-23 07:50:37 +0900

    16_TabLayout_Pager - 실습 4편

---
+++ app/src/main/java/com/example/androidwithkotlinonline/TabLayoutPagerSimpleActivity.kt
@@ -0,0 +1,81 @@
+package com.example.androidwithkotlinonline
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+
+class TabLayoutPagerSimpleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tab_layout_pager_simple)
+
+        val viewPager = findViewById<ViewPager>(R.id.viewPager)
+        val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
+
+        tabLayout.addTab(tabLayout.newTab().setText("1번째"))
+        tabLayout.addTab(tabLayout.newTab().setText("2번째"))
+        tabLayout.addTab(tabLayout.newTab().setText("3번째"))
+
+        viewPager.adapter = ViewPagerAdapter(LayoutInflater.from(this), 3)
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                viewPager.setCurrentItem(tab!!.position)
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+        })
+    }
+}
+
+class ViewPagerAdapter(
+    val layoutInflater: LayoutInflater,
+    val tabCount : Int
+) : PagerAdapter() {
+    override fun getCount(): Int {
+        return tabCount
+    }
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view === `object` as View
+        // == -> 값을 비교한다
+        // === -> 주소값을 비교한다
+        // a = 1, b = 1 -> a == b (true)
+        // a === b (true, false)  변수의 값은 같아도 주소값은 같을수도 다를수도 있다
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        container.removeView(`object` as View)
+    }
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        when (position) {
+            0 -> {
+                val view = layoutInflater.inflate(R.layout.fragment_01, container, false)
+                container.addView(view)
+                return view
+            }
+            1 -> {
+                val view = layoutInflater.inflate(R.layout.activity_phonebook_itemlist, container, false)
+                container.addView(view)
+                return view
+            }
+            else -> {
+                val view = layoutInflater.inflate(R.layout.fragment_01, container, false)
+                container.addView(view)
+                return view
+            }
+        }
+    }
+}
