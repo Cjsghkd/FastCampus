0fcfded98ee8cba269840af62c2ae123ee33e458
Cjsghkd <pjhomg5530@naver.com>
2022-06-22 23:35:35 +0900

    16_TabLayout_Pager - 실습 2편

--- app/src/main/java/com/example/androidwithkotlinonline/TabLayoutPagerActivity.kt
+++ app/src/main/java/com/example/androidwithkotlinonline/TabLayoutPagerActivity.kt
@@ -2,6 +2,9 @@ package com.example.androidwithkotlinonline
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
 
@@ -11,10 +14,45 @@ class TabLayoutPagerActivity : AppCompatActivity() {
         setContentView(R.layout.activity_tab_layout_pager)
 
         val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
-        val viewPager = findViewById<ViewPager>(R.id.pager)
+        val viewPager = findViewById<ViewPager>(R.id.viewPager)
 
+        // 탭레이아웃에 탭을 추가하는 방법
         tabLayout.addTab(tabLayout.newTab().setText("1번째"))
         tabLayout.addTab(tabLayout.newTab().setText("2번째"))
         tabLayout.addTab(tabLayout.newTab().setText("3번째"))
+
+        // Pager에 adapter를 장착하는 방법
+        viewPager.adapter = FragmentPagerAdapter(supportFragmentManager, 3)
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                viewPager.setCurrentItem(tab!!.position)
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+        })
+    }
+}
+
+class FragmentPagerAdapter(
+    fragmentManager: FragmentManager,
+    val tabCount : Int
+) : FragmentStatePagerAdapter(fragmentManager) {
+    override fun getCount(): Int {
+        return tabCount
+    }
+
+    override fun getItem(position: Int): Fragment {
+        when (position) {
+            0 -> return Fragment_01()
+            1 -> return Fragment_01()
+            else -> return Fragment_01()
+        }
     }
 }
