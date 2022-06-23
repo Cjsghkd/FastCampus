4bdc87049f01808caa41fe2d3dedf8d6a6c025c2
Cjsghkd <pjhomg5530@naver.com>
2022-06-23 21:13:22 +0900

    20_Network - 실습 2편

--- app/src/main/java/com/example/androidwithkotlinonline/NetworkActivity.kt
+++ app/src/main/java/com/example/androidwithkotlinonline/NetworkActivity.kt
@@ -1,19 +1,27 @@
 package com.example.androidwithkotlinonline
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
 
-// NetworkOnMainThreadException -> 네트워크 작업은 메인쓰레드에서 불가능
+// NetworkOnMainThreadException -> 네트워크 작업(통신)은 메인쓰레드에서 불가능
 
 class NetworkActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_network)
 
+        NetworkAsyncTask().execute()
+    }
+}
+
+class NetworkAsyncTask() : AsyncTask<Any?, Any?, Any?>() {
+    override fun doInBackground(vararg p0: Any?): Any? {
         val urlString : String = "http://mellowcode.org/json/students/" // 요청할 주소
         val url : URL = URL(urlString)
         val connection : HttpURLConnection = url.openConnection() as HttpURLConnection
@@ -30,7 +38,8 @@ class NetworkActivity : AppCompatActivity() {
                 )
             )
             buffer = reader.readLine()
+            Log.d("testt", buffer)
         }
-
+        return null
     }
 }
