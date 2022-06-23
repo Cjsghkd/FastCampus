2274da1946b41d05257d5cc1762c543f8c26f732
Cjsghkd <pjhomg5530@naver.com>
2022-06-23 16:11:53 +0900

    18_SharedPreference - 실습 2편

--- app/src/main/java/com/example/androidwithkotlinonline/SharedPreferenceActivity.kt
+++ app/src/main/java/com/example/androidwithkotlinonline/SharedPreferenceActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 
 class SharedPreferenceActivity : AppCompatActivity() {
@@ -22,7 +23,27 @@ class SharedPreferenceActivity : AppCompatActivity() {
             // MODE_MULTI_PROCESS : 이미 호출되어 사용중인지 체크
             // MODE_APPEND : 기존 preference에 신규로 추가
             val editor : SharedPreferences.Editor = sharedPreferences.edit()
-            editor.putString("key", "hello")
+            editor.putString("key1", "hello1") // key - value 방식으로 데이터를 저장
+            editor.putString("key2", "hello2")
+            editor.commit()
+        }
+        findViewById<TextView>(R.id.read).setOnClickListener {
+            val sharedPreferences = getSharedPreferences("table_name", Context.MODE_PRIVATE)
+            val valueOne = sharedPreferences.getString("key1", "Wrong1")
+            val valueTwo = sharedPreferences.getString("key2", "Wrong2")
+            Log.d("testt", valueOne!!)
+            Log.d("testt", valueTwo!!)
+        }
+        findViewById<TextView>(R.id.update).setOnClickListener {
+            val sharedPreferences = getSharedPreferences("table_name", Context.MODE_PRIVATE)
+            val editor : SharedPreferences.Editor = sharedPreferences.edit()
+            editor.putString("key1", "hello hello") // 기존에 사용했던 키와 동일한 키를 사용해서 데이터를 저장 -> Update
+            editor.commit()
+        }
+        findViewById<TextView>(R.id.delete).setOnClickListener {
+            val sharedPreferences = getSharedPreferences("table_name", Context.MODE_PRIVATE)
+            val editor : SharedPreferences.Editor = sharedPreferences.edit()
+            editor.clear()
             editor.commit()
         }
     }
