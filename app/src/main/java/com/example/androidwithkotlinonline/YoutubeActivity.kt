1f3afcc40be29c58c0266ebdfa97a3c1a9345cfa
Cjsghkd <pjhomg5530@naver.com>
2022-06-29 08:19:31 +0900

    Part4_Project - Youtube - 실습 2편 - 에러

--- app/src/main/java/com/example/androidwithkotlinonline/YoutubeActivity.kt
+++ app/src/main/java/com/example/androidwithkotlinonline/YoutubeActivity.kt
@@ -1,9 +1,18 @@
 package com.example.androidwithkotlinonline
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import retrofit2.*
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.zip.Inflater
 
 class YoutubeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,12 +30,44 @@ class YoutubeActivity : AppCompatActivity() {
                 call: Call<ArrayList<YoutubeItem>>,
                 response: Response<ArrayList<YoutubeItem>>
             ) {
-                TODO("Not yet implemented")
+                val youtubeItemList = response.body()
+//                youtubeItemList!!.forEach {
+//                    Log.d("youyou", it.title)
+//                }
             }
 
             override fun onFailure(call: Call<ArrayList<YoutubeItem>>, t: Throwable) {
-                TODO("Not yet implemented")
+                Log.d("youyou", "fail")
             }
         })
+    }
+}
+
+class YoutubeListAdapter(
+    val youtubeItemList : ArrayList<YoutubeItem>,
+    val inflater: LayoutInflater,
+    val glide : RequestManager,
+    val context : Context
+) : RecyclerView.Adapter<YoutubeListAdapter.ViewHolder>() {
+    inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        val title : TextView
+        val thumbnail : ImageView
+        val content : TextView
+    }
+
+    init {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return youtubeItemList.size
     }
 }
