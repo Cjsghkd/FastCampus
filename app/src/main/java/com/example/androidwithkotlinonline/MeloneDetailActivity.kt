0a292925a1e2207ceb7dbdfd86edff7fc1c2a772
Cjsghkd <pjhomg5530@naver.com>
2022-07-11 20:49:24 +0900

    Melon - 실습 4편

--- app/src/main/java/com/example/androidwithkotlinonline/MeloneDetailActivity.kt
+++ app/src/main/java/com/example/androidwithkotlinonline/MeloneDetailActivity.kt
@@ -1,17 +1,37 @@
 package com.example.androidwithkotlinonline
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 
 class MeloneDetailActivity : AppCompatActivity() {
+
+    lateinit var playPauseButton : ImageView
+    var is_playing : Boolean = false
+    set(value) {
+        if (value == true) {
+            playPauseButton.setImageDrawable(this.resources.getDrawable(R.drawable.pause, this.theme))
+        } else {
+            playPauseButton.setImageDrawable(this.resources.getDrawable(R.drawable.play, this.theme))
+        }
+        field = value
+    }
+
+    lateinit var melonItemList : ArrayList<MelonItem>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_melone_detail)
 
-        val melonItemList = intent.getSerializableExtra("melon_item_list") as ArrayList<MelonItem>
-        melonItemList.forEach {
-            Log.d("melonn", it.song)
+        playPauseButton = findViewById(R.id.play)
+        playPauseButton.setOnClickListener {
+            if (is_playing == true) is_playing = false
+            else is_playing = true
         }
+
+
+        melonItemList = intent.getSerializableExtra("melon_item_list") as ArrayList<MelonItem>
+
     }
 }
