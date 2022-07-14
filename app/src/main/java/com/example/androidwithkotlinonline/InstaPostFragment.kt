14b94b87f231810b7e8c3d4c0e23da413032d269
Cjsghkd <pjhomg5530@naver.com>
2022-07-14 20:04:07 +0900

    인스타그램 - 6단계 실습 1편

--- app/src/main/java/com/example/androidwithkotlinonline/InstaPostFragment.kt
+++ app/src/main/java/com/example/androidwithkotlinonline/InstaPostFragment.kt
@@ -1,17 +1,46 @@
 package com.example.androidwithkotlinonline
 
+import android.content.Intent
+import android.media.Image
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 
 class InstaPostFragment : Fragment() {
+
+    var imageUri : Uri? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.insta_post_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val selectedImageView = view.findViewById<ImageView>(R.id.selected_img)
+        val glide = Glide.with(activity as InstaMainActivity)
+
+        val ImagePickerLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            imageUri = it.data!!.data
+            glide.load(imageUri).into(selectedImageView)
+        }
+
+        ImagePickerLauncher.launch(
+            Intent(Intent.ACTION_PICK).apply {
+                this.type = MediaStore.Images.Media.CONTENT_TYPE
+            }
+        )
     }
 }
