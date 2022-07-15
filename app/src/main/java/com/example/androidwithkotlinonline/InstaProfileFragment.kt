bb60c1043831cb1a92b35a74d68d4f2a72723606
Cjsghkd <pjhomg5530@naver.com>
2022-07-15 12:19:33 +0900

    인스타그램 - 7단계 실습 4편

--- app/src/main/java/com/example/androidwithkotlinonline/InstaProfileFragment.kt
+++ app/src/main/java/com/example/androidwithkotlinonline/InstaProfileFragment.kt
@@ -1,11 +1,16 @@
 package com.example.androidwithkotlinonline
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import retrofit2.Call
@@ -58,5 +63,9 @@ class InstaProfileFragment : Fragment() {
 
             }
         })
+
+        view.findViewById<TextView>(R.id.change_img).setOnClickListener {
+            startActivity(Intent(activity as InstaMainActivity, InstaChangeProfileActivity::class.java))
+        }
     }
 }
